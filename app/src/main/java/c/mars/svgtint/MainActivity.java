25d9bb2ae15348bcbffa6fc3831c8417511e62cd
Constantine Mars <constantine.mars@gmail.com>
2015-08-30 14:23:50 +0300

    return tinted bitmap

--- app/src/main/java/c/mars/svgtint/MainActivity.java
+++ app/src/main/java/c/mars/svgtint/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Picture;
 import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.ImageView;
@@ -25,7 +28,14 @@ public class MainActivity extends ActionBarActivity {
         loadSvgWithColor(this, R.raw.inkscape, imageView, getResources().getColor(R.color.green_tint));
 
         ImageView iconView=(ImageView)findViewById(R.id.icon);
-        loadSvgWithColor(this, R.raw.sprint, iconView, getResources().getColor(R.color.orange_tint));
+        Bitmap bm= null;
+        try {
+            bm = loadSvgWithColorToBitmap(this, R.raw.play_black, iconView.getLayoutParams().width, iconView.getLayoutParams().height, getResources().getColor(R.color.orange_tint));
+            iconView.setImageBitmap(bm);
+        } catch (SVGParseException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private static boolean loadSvgWithColor(Context context, int rawId, ImageView imageView, int color){
@@ -42,6 +52,26 @@ public class MainActivity extends ActionBarActivity {
             return false;
         }
         return true;
+    }
+
+    private static Bitmap loadSvgWithColorToBitmap(Context context, int svgResId, int width, int height, int tintColor) throws SVGParseException {
+
+            SVG svg = SVG.getFromResource(context, svgResId);
+            Picture picture=svg.renderToPicture();
+
+            Bitmap originalBitmap=Bitmap.createBitmap(picture.getWidth(), picture.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas=new Canvas(originalBitmap);
+            canvas.drawPicture(picture);
+
+            Bitmap bitmapWithTint=Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            Canvas canvasWithTint=new Canvas(bitmapWithTint);
+            Paint paint = new Paint();
+            paint.setColorFilter(new PorterDuffColorFilter(tintColor, PorterDuff.Mode.SRC_IN));
+
+            Rect dst = new Rect(0, 0, width, height);
+            canvasWithTint.drawBitmap(originalBitmap, null, dst, paint);
+
+        return bitmapWithTint;
     }
 
 }
